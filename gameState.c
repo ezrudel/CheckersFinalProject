1003c5020b64ad1c0cae9b05ed92ce64e336b6e7
ezrudel <erudel@oberlin.edu>
2021-05-08 16:59:14 -0400

    almost done with capture functionality

--- gameState.c
+++ gameState.c
@@ -44,48 +44,6 @@ int getType(struct pos* p) { // returns type of square at position p
 	return board[p->x][p->y];
 }
 
-int isMyPiece(struct pos* p, int side) { // returns 1 if p contains a piece belonging to side, returns 0 otherwise
-	int type = getType(p);
-	if (type == side || type == 2+side) {
-		return 1;
-	}
-	return 0;
-}
-
-struct pos** getMoves(struct pos* p) { // returns an array of pointers to positions to which the piece at p can move, terminated by a null pointer, returns null if p is not a piece
-	if (getType(p) < 1) {
-		return NULL;
-	}
-	int i = 0;
-	struct pos** moves = malloc(5 * sizeof(struct pos*));;
-	if ((getType(p) == COMP || isKing(p)) && (p->y < SIZE-1)) {
-		if ((board[(p->x)-1][(p->y)+1] == BLACK) && (p->x > 0)) {
-			struct pos* q = pos((p->x)-1, (p->y)+1);
-			moves[i] = q;
-			i++;
-		}	
-		if ((board[(p->x)+1][(p->y)+1] == BLACK) && (p->x < SIZE-1)) {
-			struct pos* q = pos((p->x)+1, (p->y)+1);
-			moves[i] = q;
-			i++;
-		}
-	}
-	if ((getType(p) == USER || isKing(p)) && (p->y > 0)) {	
-		if ((board[(p->x)-1][(p->y)-1] == BLACK) && (p->x > 0)) {
-			struct pos* q = pos((p->x)-1, (p->y)-1);
-			moves[i] = q;
-			i++;
-		}	
-		if ((board[(p->x)+1][(p->y)-1] == BLACK) && (p->x < SIZE-1)) {
-			struct pos* q = pos((p->x)+1, (p->y)-1);
-			moves[i] = q;
-			i++;
-		}
-	}
-	moves[i] = NULL;
-	return moves;
-}
-
 void king() { // checks the whole board for pieces that are on their opponents starting line and kings them
 	for (int x = 0; x < SIZE; x++) {
 		if (board[x][0] == USER) {
@@ -127,21 +85,66 @@ int containsPos(struct pos** squares, struct pos* square) { // returns 1 if squa
 	return 0;
 }
 
+int canMove(struct pos* p, int x, int y) { // determines whether the piece at p can move in the given direction <x,y> (x and y will be 1 or -1)
+	struct pos* to = pos(p->x+x, p->y+y);
+	return inBounds(to) && (getType(to) == BLACK);
+}
+
+struct pos** getMoves(struct pos* p) { // returns an array of pointers to places p can move, terminated by a null pointer, returns null if p is not a piece
+	if (getType(p) < 1) {
+		return NULL;
+	}
+	int i = 0;
+	struct pos** moves = malloc(5 * sizeof(struct pos*));;
+	if ((getType(p) == COMP) || isKing(p)) {
+		if (canMove(p, -1, 1)) {
+			struct pos* q = pos((p->x)-1, (p->y)+1);
+			moves[i] = q;
+			i++;
+		}	
+		if (canMove(p, 1, 1)) {
+			struct pos* q = pos((p->x)+1, (p->y)+1);
+			moves[i] = q;
+			i++;
+		}
+	}
+	if ((getType(p) == USER) || isKing(p)) {	
+		if (canMove(p, -1, -1)) {
+			struct pos* q = pos((p->x)-1, (p->y)-1);
+			moves[i] = q;
+			i++;
+		}	
+		if (canMove(p, 1, -1)) {
+			struct pos* q = pos((p->x)+1, (p->y)-1);
+			moves[i] = q;
+			i++;
+		}
+	}
+	moves[i] = NULL;
+	return moves;
+}
+
 int move(struct pos* from, struct pos* to) { // moves the piece at pos from to pos to. returns 1 if successful, 0 if to is not a valid destination, and -1 if there are no valid destinations
 	struct pos** moves = getMoves(from);
+	struct pos** takes = getTakes(from);
 	if (moves == NULL) {
 		fprintf(stderr, "error: no piece at %d,%d", from->x, from->y);
 		return -1;
 	}
-	if (moves[0] == NULL) {
+	if ((moves[0] == NULL) && (takes[0] == NULL)) {
 		return -1;
 	}
-	if (!containsPos(getMoves(from), to)) {
-		return 0;
+	if (containsPos(getMoves(from), to)) {
+		board[to->x][to->y] = getType(from);
+		board[from->x][from->y] = BLACK;
+		return 1;
 	}
-	board[to->x][to->y] = getType(from);
-	board[from->x][from->y] = BLACK;
-	return 1;
+	if (containsPos(getTakes(from), to)) {	
+		board[to->x][to->y] = getType(from);
+		board[from->x][from->y] = BLACK;
+
+		return 1;
+	}
 }
 
 int side(struct pos* p) { // returns the side that p is on, or 0/-1 if p is not a piece
@@ -162,37 +165,46 @@ int opp(int side) { // returns the opposite side of side
 	}
 }
 
+int inBounds(struct pos* p) { // determines whether p is within the board
+	return (p->x >= 0) && (p->x < SIZE) && (p->y >= 0) && (p->y < SIZE);
+}
+
+int canTake(struct pos* p, int x, int y) { // determines whether the piece at p can take another piece in the given direction <x,y> (x and y will be 1 or -1)
+	struct pos* one = pos(p->x+x, p->y+y);
+	struct pos* two = pos(p->x+(x*2), p->y+(y*2));
+	return inBounds(two) && (getType(two) == BLACK) && (side(one) == opp(side(p)));
+}
+
 struct pos** getTakes(struct pos* p) { // returns an array of pointers to pieces that p can take, terminated by a null pointer, returns null if p is not a piece
 	if (getType(p) < 1) {
 		return NULL;
 	}
 	int i = 0;
-	int mySide = side(p);
-	struct pos** moves = malloc(5 * sizeof(struct pos*));;
-	if ((mySide == COMP || isKing(p)) && (p->y < SIZE-1)) {
-		if (side(pos(p->x-1, p->y+1) == opp(mySide)) && (p->x > 0)) {
+	struct pos** takes = malloc(5 * sizeof(struct pos*));;
+	if ((getType(p) == COMP) || isKing(p)) {
+		if (canTake(p, -1, 1)) {
 			struct pos* q = pos((p->x)-1, (p->y)+1);
-			moves[i] = q;
+			takes[i] = q;
 			i++;
 		}	
-		if ((board[(p->x)+1][(p->y)+1] == BLACK) && (p->x < SIZE-1)) {
+		if (canTake(p, 1, 1)) {
 			struct pos* q = pos((p->x)+1, (p->y)+1);
-			moves[i] = q;
+			takes[i] = q;
 			i++;
 		}
 	}
-	if ((getType(p) == USER || isKing(p)) && (p->y > 0)) {	
-		if ((board[(p->x)-1][(p->y)-1] == BLACK) && (p->x > 0)) {
+	if ((getType(p) == USER) || isKing(p)) {	
+		if (canTake(p, -1, -1)) {
 			struct pos* q = pos((p->x)-1, (p->y)-1);
-			moves[i] = q;
+			takes[i] = q;
 			i++;
 		}	
-		if ((board[(p->x)+1][(p->y)-1] == BLACK) && (p->x < SIZE-1)) {
+		if (canTake(p, 1, -1)) {
 			struct pos* q = pos((p->x)+1, (p->y)-1);
-			moves[i] = q;
+			takes[i] = q;
 			i++;
 		}
 	}
-	moves[i] = NULL;
-	return moves;
+	takes[i] = NULL;
+	return takes;
 }
--- gameState.h
+++ gameState.h
@@ -12,20 +12,41 @@
 #define COMP_K 3 // computer's king
 #define USER_K 4 // user's king
 
-void initBoard();
-void startGame();
-void printBoard();
-struct pos* pos(int x, int y);
-int getType(struct pos* p);
-int isMyPiece(struct pos* p, int side);
-struct pos** getMoves(struct pos* p);
-void king();
-int isKing(struct pos* p);
-int containsPos(struct pos** squares, struct pos* square);
-int move(struct pos* from, struct pos* to);
 
-struct pos {
+void initBoard(); // initializes the board with black and red squares
+
+void startGame(); // puts pieces in their starting positions
+
+void printBoard(); // prints the int values of the board
+
+struct pos* pos(int x, int y); // returns a pos struct with values x and y
+
+int getType(struct pos* p); // returns the value of board at p
+
+struct pos** getMoves(struct pos* p); // returns an array of pointers to places p can move (without taking), terminated by a null pointer, returns null if p is not a piece
+
+void king(); // kings all pieces on the board that are on their opponent's starting line
+
+int isKing(struct pos* p); // determines whether p contains a king piece
+
+int containsPos(struct pos** squares, struct pos* square); // determines whether squares contains an identical struct pos to square
+
+int move(struct pos* from, struct pos* to); // moves the piece at from to to. returns 1 if successful, 0 if to is not in the range of from, and -1 if there are no valid moves from from, or if from does not contain a piece
+
+int side(struct pos* p); // returns the side that the piece at p is on. if there is no piece at p, returns the value at p
+
+int opp(int side); // returns the opposite side of side
+
+int inBounds(struct pos* p); // determines whether p is within the board
+
+int canTake(struct pos* p, int x, int y); // determines whether the piece at p can take another piece in the given direction x,y (x and y will be 1 or -1)
+
+struct pos** getTakes(struct pos* p); // returns an array of pointers to pieces that p can take, terminated by a null pointer, returns null if p is not a piece
+
+
+struct pos { // represents the position on the board (x,y) -- ONLY PASS POINTERS TO THIS STRUCT, NOT THE STRUCT ITSELF
 	int x, y;
 };
+
 
 #endif
